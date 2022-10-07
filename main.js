0226d2b2bc7c2638b0d44de1653a9c700b938194
1977Suryaa <101641242+1977Suryaa@users.noreply.github.com>
2022-10-07 15:57:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 function setup(){
-    canvas=createCanvas(400,400)
+    canvas=createCanvas(500,400)
     canvas.center()
+    webcam=createCapture(VIDEO)
+    webcam.hide()
+    poseNet = ml5.poseNet(webcam, modelLoaded);
+    poseNet.on("pose",gotpose)
 }
 function draw(){
+    image(webcam,0,0,500,400)
     
+
+}
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+function gotpose(results){
+    if(results.length>0){
+        console.log(results)
+        var ms1=results[0].pose.nose.x
+        var ms2=results[0].pose.nose.y
+    }
+
+}
+
+function photo(){
+    save("face filter.png")
 }
